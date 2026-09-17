44bbda7c6912cd7279e090375c6cbe13888cc9b0
joaobatista52 <304668078+joaobatista52@users.noreply.github.com>
2026-09-17 17:24:00 -0300

    remover artefatos de diagnostico e restaurar configuracao limpa com qa aprovado

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,4 @@
 /* PostCSS Config file: https://postcss.org */
-import fs from 'node:fs'
-if (fs.existsSync('public/og-image.png')) {
-  const buf = fs.readFileSync('public/og-image.png')
-  const width = buf.readUInt32BE(16)
-  const height = buf.readUInt32BE(20)
-  const bitDepth = buf[24]
-  const colorType = buf[25]
-  throw new Error(`OG_IMAGE_INSPECT: size=${buf.length}, width=${width}, height=${height}, bitDepth=${bitDepth}, colorType=${colorType}`)
-}
-
 export default {
   plugins: {
     tailwindcss: {},
